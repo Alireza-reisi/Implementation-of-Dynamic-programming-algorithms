173d1aab2ec48e5ced5795d198a44f1176b328b5
alireza reisi <alireza.reisi03127@gmail.com>
2023-11-14 16:46:12 +0330

    implementation using just 3 variable

--- dynamic-programing-fibonacci.py
+++ dynamic-programing-fibonacci.py
@@ -1,14 +1,15 @@
 def Fibonacci(n):
-    fib=[None for i in range(0,n+1)]
-    fib[0]=0
-    fib[1]=1
-    for j in range (2,n+1):
-        fib[j]=fib[j-1]+fib[j-2]
-    
-    return fib[n]
+    fib=[0,1,None]
+    if n<2:
+        return fib[n]
+    else:
+        for i in range (2,n+1):
+            fib[2]=fib[0]+fib[1]
+            fib[0]=fib[1]
+            fib[1]=fib[2]
+    return fib[2]
     
 
 
 n=int(input("Enter the desired sentence number: "))
-x=Fibonacci(n)
-print(x)
+print(Fibonacci(n))
