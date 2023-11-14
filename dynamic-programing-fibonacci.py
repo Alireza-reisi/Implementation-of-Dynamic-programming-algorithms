1f88b6ce13d295a0587b8010cabc815d42aca8ba
alireza reisi <alireza.reisi03127@gmail.com>
2023-11-14 16:39:43 +0330

    implementation without using python methods

--- dynamic-programing-fibonacci.py
+++ dynamic-programing-fibonacci.py
@@ -1,10 +1,14 @@
 def Fibonacci(n):
-    fib=[1,1]
-    for i in range (1,n):
-        fib.append(fib[i]+fib[i-1])
-    return fib[-1]
+    fib=[None for i in range(0,n+1)]
+    fib[0]=0
+    fib[1]=1
+    for j in range (2,n+1):
+        fib[j]=fib[j-1]+fib[j-2]
+    
+    return fib[n]
     
 
 
 n=int(input("Enter the desired sentence number: "))
-print(Fibonacci(n))
+x=Fibonacci(n)
+print(x)
