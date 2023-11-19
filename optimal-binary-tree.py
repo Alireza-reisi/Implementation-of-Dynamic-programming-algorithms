0dbeb53f00679a351f15b9870265c84c1f0dca5e
alireza reisi <alireza.reisi03127@gmail.com>
2023-11-19 13:37:05 +0330

    optimal binery tree's matrixes

---
+++ optimal-binary-tree.py
@@ -0,0 +1,40 @@
+def MinMult(key,frequency,n):
+    optimal_key=[[0 for i in range(n+1)] for j in range(n+1)]
+    optimal_weight=[[0 for i in range(n+1)] for j in range(n+1)]
+    for x in range(1,n+1):
+        for i in range(0,n-x+1):
+            j=i+x
+            min=float('inf')
+            k=i+1
+            weight=0
+            for w in range(i,j):
+                weight+=frequency[w]
+            while k<=j:
+                z=optimal_weight[i][k-1]+optimal_weight[k][j]+weight
+                if z<min:
+                    min=z
+                    optimal_key[i][j]=k
+                k=k+1
+            optimal_weight[i][j]=min
+    return optimal_weight,optimal_key
+
+
+
+
+# ========================================================================
+
+key=[10,20,30,40]
+frequency=[4,2,6,3]
+optimal_weight,optimal_key=MinMult(key,frequency,len(key))
+
+# ========================================================================
+
+for i in range(len(key)):
+    print(optimal_key[i]) 
+
+print("=============================")
+
+for i in range(len(key)):
+    print(optimal_weight[i]) 
+
+print("=============================")
